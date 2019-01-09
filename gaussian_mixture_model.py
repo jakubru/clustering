386bc6d1fed3b9f664e84291f75ab07e06534004
SteelHawX <bart.gniady@gmai.com>
2019-01-09 19:02:59 +0100

    commit

--- gaussian_mixture_model.py
+++ gaussian_mixture_model.py
@@ -1,11 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-def gaussian_mixture_model():
-    pass
+def gaussian_mixture_model(dataset, indicies, mi, K):
+    means_k = []
+    c_k = []
+    sigma_k = []
+    clusters = zip(dataset, indicies)
+    for i in range(K):
+        c_k.append(len(list(filter(lambda el: el == i, indicies))))
+    for i in range (K):
+        means_k.append(sum([pair[0] for pair in filter(lambda el: el[1] == i, clusters)])/c_k[i])
+    for i in range(K):
+        filtered = [pair[0] for pair in filter(lambda el: el[1] == i, clusters)]
+        cov = 0
+        print(filtered)
+        for element in filtered:
+            cov += (element - means_k[i])*(element - means_k[i]).tanspose()
+            print(cov)
+        cov = cov/c_k[i]
+        sigma_k.append(cov)
+    print(sigma_k)
 
 
-def fit():
-    pass
-
-def visualize():
-    pass
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy.stats as stats
 import k_means
+import gaussian_mixture_model
 
 gauss1 = stats.multivariate_normal([0, 0], [[20, 0], [0, 20]])
 gauss2 = stats.multivariate_normal([12, 12], [[3, 0], [0, 3]])
@@ -17,4 +18,6 @@ dataset = np.array(dataset)
 
 
 indicies, mi = k_means.fit(3,dataset)
-k_means.visualize(dataset, indicies, mi)
+#k_means.visualize(dataset, indicies, mi)
+
+gaussian_mixture_model.gaussian_mixture_model(dataset, indicies, mi, 3)
